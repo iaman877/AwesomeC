6aefd4344b29ac0e58980da021aa8d4c66a4d71b
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-04 14:14:08 +0530

    Initial

---
+++ Pattern Printing/pro3.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int i,j; 
+    char k; 
+    for(i=1;i<=4;i++){ 
+         k='A'; 
+          for(j=1;j<=7;j++){ 
+            if(j<=5-i||j>=3+i){ 
+               printf("%c",k); 
+                k++; 
+            } 
+            else  
+               printf(" "); 
+          } 
+           printf("\n"); 
+    } 
+} 
