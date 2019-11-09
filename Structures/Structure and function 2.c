cabec0fbf84ba60a4278bda9b955c6b1eddbbf5c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-09 11:05:05 +0530

    Passing indivial member

--- Structures/Structure and function 2.c
+++ Structures/Structure and function 2.c
@@ -7,9 +7,9 @@ typedef struct
 void display(int , int); 
 int main() 
 { 
-    POINT p1 = {2,3}; 
-    display(p1.x, p1.y); 
-    return 0; 
+     POINT p1 = {2,3}; 
+     display(p1.x, p1.y); 
+     return 0; 
 } 
 void display(int a, int b) 
 { 
