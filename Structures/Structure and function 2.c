dc78b797b40895e497821b3f3ed3eff687db5bef
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-09 11:02:58 +0530

    Initial

---
+++ Structures/Structure and function 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+typedef struct 
+{ 
+    int x; 
+    int y; 
+}POINT; 
+void display(int , int); 
+int main() 
+{ 
+    POINT p1 = {2,3}; 
+    display(p1.x, p1.y); 
+    return 0; 
+} 
+void display(int a, int b) 
+{ 
+    printf("\n The cordinates of a points is %d %d ",a,b); 
+} 
