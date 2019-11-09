2e40ae7546686737e6fb7a678f8a235342839d64
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-09 11:01:58 +0530

    Initial

---
+++ Structures/Structure and function 1.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+struct student 
+{ 
+    char name[20]; 
+    int roll_no; 
+    int marks; 
+}; 
+ void print_struct(char name[], int roll_no, int marks); 
+ int main() 
+{ 
+    struct student stu = {"Tim", 1, 78}; 
+    print_struct(stu.name, stu.roll_no, stu.marks); 
+    return 0; 
+} 
+ void print_struct(char name[], int roll_no, int marks) 
+{ 
+    printf("Name: %s\n", name); 
+    printf("Roll no: %d\n", roll_no); 
+    printf("Marks: %d\n", marks); 
+    printf("\n"); 
+} 
