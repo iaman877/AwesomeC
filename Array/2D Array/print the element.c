f94769063f6c10578e06e5e1c8bfc95cbe90702c
iaman877 <amanbhardwaj.gps@gmail.com>
2021-03-29 10:29:36 +0530

    Recovering From existing repo

---
+++ Array/2D Array/print the element.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int arr[2][2] = {12,34,56,32}; 
+    int i,j; 
+    for(i=0;i<2;i++) 
+    { 
+        printf("\n"); 
+        for(j=0;j<2;j++) 
+        printf("%d\t",arr[i][j]); 
+    } 
+    return 0;  
+} 
